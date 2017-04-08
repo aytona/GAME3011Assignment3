4675593d2743bd01010022decc41fe733fd1a8aa
Christopher Aytona <christopher.aytona@gmail.com>
2017-04-08 17:38:41 -0400

    Singletons

---
+++ Assets/Scripts/SingletonClass.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class SingletonClass<T> : MonoBehaviour where T : MonoBehaviour {
+
+    private static T instance;
+
+    public static T Instance {
+        get {
+            if (instance == null) {
+                instance = FindObjectOfType<T>();
+                if (instance == null) {
+                    GameObject singleton = new GameObject(typeof(T).Name);
+                    instance = singleton.AddComponent<T>();
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake() {
+        if (instance == null) {
+            instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/SingletonGameObject.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class SingletonGameObject : MonoBehaviour {
+
+    private static GameObject instance;
+
+    public static GameObject Instance {
+        get {
+            if (instance == null) {
+                instance = GameObject.Find(instance.name);
+                if (instance == null) {
+                    Instantiate(instance);
+                }
+            }
+            return instance;
+        }
+    }
+
+    public virtual void Awake() {
+        if (instance == null) {
+            instance = gameObject;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+        }
+    }
+}
